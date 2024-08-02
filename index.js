47f619d720675743602bea5428ff6304435e8c95
Koala Dude <147400996+Foibos-Saloufakos@users.noreply.github.com>
2024-08-02 21:30:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Monitoring Synergy Research Institute"];
+const statusMessages = ["Monitoring", "Protecting", "Synergy Research Institute"];
 
 
 let currentIndex = 0;
